7d93d0b765973d444cb57f56c644daa233deb9f3
Jan Schumann <js@schumann-it.com>
2025-07-15 10:55:39 +0200

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -11,35 +11,6 @@ export default defineConfig(({ mode }) => ({
   build: {
     sourcemap: mode === "development",
     chunkSizeWarningLimit: 1000, // Increase warning limit to 1MB
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Vendor chunks
-          'react-vendor': ['react', 'react-dom'],
-          'react-admin-vendor': ['react-admin', 'ra-core', 'ra-ui-materialui', 'ra-i18n-polyglot', 'ra-language-english'],
-          'mui-vendor': ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
-          'auth-vendor': ['@azure/msal-react', 'ra-auth-msal'],
-          'router-vendor': ['react-router', 'react-router-dom'],
-          'utils-vendor': ['react-markdown'],
-          'data-vendor': ['ra-data-simple-rest'],
-        },
-        // Optimize chunk naming
-        chunkFileNames: 'js/[name]-[hash].js',
-        entryFileNames: 'js/[name]-[hash].js',
-        assetFileNames: (assetInfo) => {
-          const name = assetInfo.name || 'asset';
-          const info = name.split('.');
-          const ext = info[info.length - 1];
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
-            return `img/[name]-[hash][extname]`;
-          }
-          if (/css/i.test(ext)) {
-            return `css/[name]-[hash][extname]`;
-          }
-          return `assets/[name]-[hash][extname]`;
-        },
-      },
-    },
   },
   // This allows to have sourcemaps in production. They are not loaded unless you open the devtools
   // Remove this line if you don't need to debug react-admin in production
@@ -49,7 +20,6 @@ export default defineConfig(({ mode }) => ({
       ...getAliasesToDebugInProduction(),
     },
   },
-  base: "./",
 }));
 
 function getAliasesToDebugInProduction() {
