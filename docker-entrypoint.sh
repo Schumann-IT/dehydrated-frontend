45136be0d126763638d5c1a78af94ef19447f5cc
Jan Schumann <js@schumann-it.com>
2025-07-16 16:14:58 +0200

    fix: remove unecessary envsub

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,5 @@
 # Generate env-config.js from environment variables
 ./generate-env-config.sh
 
-# Substitute environment variables in nginx configuration
-envsubst '$VITE_APP_BASE_URI $VITE_API_BASE_URL $VITE_ENABLE_MSAL $VITE_THEME_NAME $VITE_THEME_MODE $VITE_MSAL_CLIENT_ID $VITE_MSAL_AUTHORITY $VITE_DEHYDRATED_API_IDENTIFIER' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
 # Start nginx
 exec nginx -g "daemon off;" 
